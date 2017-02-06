27769b5666505495becf335fcf47bb8be2daf22d
David Forster <dahidforster@googlemail.com>
2017-02-07 00:07:54 +0200

    First upload
    main menu has been completed

---
+++ RUH Diabetes Project/Assets/Scripts/mainMenuNav.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainMenuNav : MonoBehaviour {
+
+    public void UserSelectPlay() // takes user back to the main menu
+    {
+        SceneManager.LoadScene("yearSelection"); // if questions done = all of them, load results screen
+    }
+
+    public void UserSelectQuit() // takes user back to the main menu
+    {
+        Application.Quit();
+    }
+
+}
