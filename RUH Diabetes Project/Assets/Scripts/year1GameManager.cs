72255197afc5dafd0bb7a8eee6c6f67f59e5e7ac
David Forster <dahidforster@googlemail.com>
2017-02-27 18:40:13 +0200

    Attempting to fix image issue
    Updated version

---
+++ RUH Diabetes Project/Assets/Scripts/year1GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+public class year1GameManager : MonoBehaviour {
+
+    public year1Quiz[] imagePanel;
+    private static List<year1Quiz> unansweredQuestions;
+    private year1Quiz currentQuestion;
+
+    [SerializeField]
+    Image questionImage; // question text
+
+    void Start()
+    {
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = imagePanel.ToList<year1Quiz>();
+        }
+
+        SetRandomImage();
+        Debug.Log(currentQuestion.image + " is " + currentQuestion.isCorrect);
+    }   
+    void SetRandomImage()
+    {
+        int randomImageIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomImageIndex];
+
+        questionImage = currentQuestion.image; 
+
+        unansweredQuestions.RemoveAt(randomImageIndex);   
+    }
+}
---
+++ RUH Diabetes Project/Assets/Scripts/year1Quiz.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class year1Quiz {
+
+    public Image image;
+    public bool isCorrect;
+}
